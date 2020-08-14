0b6cf7c202a11e018e85ab4e409782e67c108b26
zhoushichuan <zhoushichuan@lazyaudio.com>
2020-08-14 12:00:15 +0800

    zhou

--- login/templates/admin/platform/test.py
+++ login/templates/admin/platform/test.py
@@ -1,11 +1,17 @@
 from login.templates.admin.platform.common.operate_mysql import billing_select
 
 
-def testtt():
-    # company_subsidy_percent = billing_select("select * from p_config_server pcs where config_key ='COMPANY_SUBSIDY_PERCENT';", 'billing')
-    a='12.09'
-    b=a[0]
-    print(b)
+def main():
+    sum = 0  # 定义变量做累加器
 
-if __name__=='__main__':
-    testtt()
+    n = int(input('n='))  # 从键盘上输入累加的范围
+
+    for x in range(n):
+        sum += (x + 1)
+
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
+
